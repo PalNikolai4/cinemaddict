693b39899ae84282abd66d4af8b19808229cc3a3
PalNik <paha-87@mail.ru>
2025-10-23 18:36:35 +1000

    Task 1.3
    Created modules 'films-presenter.js'in 'src/presenter/

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { render } from './render.js';
+import render from './render.js';
 import HeaderProfileView from './view/header-profile-view.js';
+import FilterView from './view/filter-view.js';
+import SortView from './view/sort-view.js';
+import FilmCardView from './view/films-view.js';
+
 import FilmButtonMoreView from './view/film-button-more-view.js';
 
+
+const siteMainElement = document.querySelector('.main');
+
 render(new HeaderProfileView(), document.querySelector('.header'));
-render(new FilmButtonMoreView(), document.querySelector('.main'));
+// render(new FilterView(), siteMainElement);
+// render(new SortView(), siteMainElement);
+// render(new FilmCardView(), siteMainElement);
+
+// render(new FilmButtonMoreView(), siteMainElement);
