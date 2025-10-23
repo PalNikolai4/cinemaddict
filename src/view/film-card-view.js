1ce851d9731c0d821abb5c8d8e698e9d326a2d6c
PalNik <paha-87@mail.ru>
2025-10-23 11:49:40 +1000

    Task 1.3
    Connected fn 'createFilmCardInfoTemplate' from module 'film-card-info-template.js' and fn'createFilmCardControlsTemplate' from module 'film-card-controls-template.js'
     to fn 'createFilmCardTemplate' in module 'film-card-view.js'.

--- src/view/film-card-view.js
+++ src/view/film-card-view.js
@@ -1,6 +1,12 @@
 import {createElement} from '../render';
+import {createFilmCardInfoTemplate} from './film-card-info-template';
+import {createFilmCardControlsTemplate} from './film-card-controls-template';
 
-const createFilmCardTemplate = () => '';
+const createFilmCardTemplate = () =>
+  `<article class="film-card">
+    ${createFilmCardInfoTemplate()}
+    ${createFilmCardControlsTemplate()}
+  </article>`;
 
 class FilmCardView {
   getTemplate() {
