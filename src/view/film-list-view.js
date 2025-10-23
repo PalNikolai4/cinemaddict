c38919b501132cd55abb384503f4e200841fd6e4
PalNik <paha-87@mail.ru>
2025-10-23 10:54:09 +1000

    Task 1.3
    Created module 'film-list-view.js' in 'src/view/' and in it:
    	- fn 'createFilmListViewTemplate';
    	- class 'FilmListView'.

---
+++ src/view/film-list-view.js
@@ -0,0 +1,25 @@
+import { createElement } from '../render';
+
+const createFilmListViewTemplate = () =>
+  `<section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+  </section>`;
+
+class FilmListView {
+  getTemplate() {
+    return createFilmListViewTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate);
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
+
+export default FilmListView;
