913f6fbd981be0a8f227d052e0c254e868ad5e05
PalNik <paha-87@mail.ru>
2025-10-25 14:24:35 +1000

    Task 1.3 [film details info template]
    Created module 'film-details-info-template.js' in 'src/view/' and in it:
    - fn 'createFilmDetailsInfoTemplate';
    - imported into 'film-details-view.js' in fn 'createFilmDetailsTemplate'.

--- src/view/film-details-view.js
+++ src/view/film-details-view.js
@@ -1,26 +1,25 @@
 import {createElement} from '../render';
-// import {createFilmDetailsInfoTemplate} from './film-details-info-template.js'
+import {createFilmDetailsInfoTemplate} from './film-details-info-template';
 // import {createFilmDetailsControlsTemplate} from './film-details-controls-template.js'
 // import {createFilmDetailsCommentsTemplate} from './film-details-comments-template.js'
 // import {createFilmDetailsNewCommentTemplate} from './film-details-new-comment-template.js'
 
 const createFilmDetailsTemplate = () =>
-  `
-  <section class="film-details">
+  `<section class="film-details">
     <form class="film-details__inner" action="" method="get">
       <div class="film-details__top-container">
         <div class="film-details__close">
           <button class="film-details__close-btn" type="button">close</button>
         </div>
         ${createFilmDetailsInfoTemplate()}
-        ${createFilmDetailsControlsTemplate()}
+        ${createFilmDetailsControlsTemplate}
       </div>
 
       <div class="film-details__bottom-container">
         <section class="film-details__comments-wrap">
           <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
-          ${createFilmDetailsCommentsTemplate()}
-          ${createFilmDetailsNewCommentTemplate()}
+          ${createFilmDetailsCommentsTemplate}
+          ${createFilmDetailsNewCommentTemplate}
         </section>
       </div>
     </form>
